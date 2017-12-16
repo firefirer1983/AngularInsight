ded2df29d4ea3143c8cd6ff505870ea600f794b5
fyman.zhang <fyman.zhang@icloud.com>
2017-12-16 15:30:26 +0800

    create project

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularInsight')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
